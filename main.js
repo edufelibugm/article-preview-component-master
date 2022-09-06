af1033da328b62a17a0b496887153665514fa566
edufelibugm <106450903+edufelibugm@users.noreply.github.com>
2022-09-06 08:58:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.share').addEventListener('click', () => {
+    document.querySelector('.desplegable').classList.add('show')
+})
+document.querySelector('.desactive').addEventListener('click', () => {
+    document.querySelector('.desplegable').classList.remove('show')
+})
